d23c6c69c92ce6bf1531e37017429f63b00aa238
Alda Vigdís Skarphéðinsdóttir <alda.vigdis@a8c.com>
2019-01-25 16:02:17 +0100

    Adding the SVG stuff from Thomas as well as 350px max width

--- client/gutenberg/extensions/related-posts/edit.jsx
+++ client/gutenberg/extensions/related-posts/edit.jsx
@@ -24,10 +24,27 @@ function PlaceholderPostEdit( props ) {
 			</h3>
 			{ props.displayThumbnails && (
 				<figure
-					className="jp-related-posts-i2__post-image-placeholder-icon"
+					className="jp-related-posts-i2__post-image-placeholder"
 					aria-label={ __( 'Placeholder image' ) }
 				>
-					<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<SVG
+						className="jp-related-posts-i2__post-image-placeholder-square"
+						xmlns="http://www.w3.org/2000/svg"
+						width="100%"
+						height="100%"
+						viewBox="0 0 350 200"
+					>
+						<title>{ __( 'Grey square' ) }</title>
+						<Path d="M0 0h350v200H0z" fill="#8B8B96" fill-opacity=".1" />
+					</SVG>
+					<SVG
+						className="jp-related-posts-i2__post-image-placeholder-icon"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<title>{ __( 'Icon for image' ) }</title>
 						<Path fill="none" d="M0 0h24v24H0V0z" />
 						<Path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z" />
 					</SVG>
